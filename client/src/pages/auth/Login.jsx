2e1dca2ff4dabd597ccb11db6e59b8442e81d637
ramishka <eg245369@engug.ruh.ac.lk>
2026-01-30 15:38:49 +0530

    checking

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -63,7 +63,7 @@ const Login = () => {
       {/* Login Card */}
       <div className="w-full max-w-md bg-white rounded-2xl shadow-lg p-8 border border-gray-100">
         <h2 className="text-2xl font-bold text-green-700 mb-6 text-center">
-          Log In to Your Account
+          Log In to Your Accountss
         </h2>
 
         <form onSubmit={handleSubmit} className="space-y-6">
